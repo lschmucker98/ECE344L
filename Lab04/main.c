0b1d2d2c083dbf20ce86d35374bd0e1d2482d54b
Landon_Laptop <lschmucker@unm.edu>
2020-03-19 18:36:38 -0600

    library and header

--- Lab04/main.c
+++ Lab04/main.c
@@ -1,1 +1,25 @@
+/************************************************************************/
+/* ECE 344L - Microprocessors – Spring 2020                             */
+/*                                                                      */
+/*                                                                      */
+/* Lab04.c -- General Description of file                               */
+/*                                                                      */
+/*                                                                      */
+/************************************************************************/
+/* Author(s): Landon Schmucker                                          */
+/*            Following functions from Edward Nava:                     */
+/*             DeviceInit()                                             */
+/*             DisplayInit()                                            */
+/*             DelayMS()                                                */
+/************************************************************************/
+/*   Detailed File Description:                                         */
+/* Vending machine simulator using buttons and LEDs on the MX7 board.   */
+/* Every item is 30 cents, only nickels and dimes.                      */
+/*                                                                      */
+/************************************************************************/
+/*  Revision History: 2/29/2020 Started File                            */
+/*                    3/01/2020 Started state machine                   */
+/*                    3/02/2020 Debug                                   */
+/************************************************************************/
+
 #include <plib.h>
